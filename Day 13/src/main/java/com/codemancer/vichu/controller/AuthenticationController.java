386ac050ffa95e513d8ef4428e99aad60d905736
vijayaprabha2003 <104961158+vijayaprabha2003@users.noreply.github.com>
2023-10-05 10:45:43 +0530

    Add files via upload

---
+++ Day 13/src/main/java/com/codemancer/vichu/controller/AuthenticationController.java
@@ -0,0 +1,37 @@
+package com.codemancer.vichu.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codemancer.vichu.constant.Api;
+import com.codemancer.vichu.dto.request.AuthenticationRequest;
+import com.codemancer.vichu.dto.request.RegisterRequest;
+import com.codemancer.vichu.dto.response.AuthenticationResponse;
+import com.codemancer.vichu.service.AuthService;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping(Api.AUTH)
+@RequiredArgsConstructor
+@Tag(name = "Authentication")
+public class AuthenticationController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
+        boolean isRegistered = authService.userRegistration(request);
+        return isRegistered ? ResponseEntity.ok("User registered successfully")
+                : ResponseEntity.badRequest().body("Sommething went wrong!");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+        return ResponseEntity.ok(authService.userAuthentication(request));
+    }
+}
---
+++ Day 13/src/main/java/com/codemancer/vichu/service/impl/UserServiceImpl.java
@@ -0,0 +1,80 @@
+package com.codemancer.vichu.service.impl;
+
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.codemancer.vichu.dto.request.UserRequest;
+import com.codemancer.vichu.dto.response.UserResponse;
+import com.codemancer.vichu.model.User;
+import com.codemancer.vichu.model.enumerate.Role;
+//import com.iamneo.ecom.repository.OrderRepository;
+import com.codemancer.vichu.repository.UserRepository;
+import com.codemancer.vichu.service.UserService;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+ 
+
+    public List<UserResponse> getAllUsers() {
+        List<User> userList = userRepository.findAll();
+        return userList.stream()
+                .filter(user -> !user.getRole().equals(Role.ADMIN))
+                .map(this::mapUserToUserResponse)
+                .collect(Collectors.toList());
+    }
+
+    public UserResponse getUser(Long uid) {
+        User user = userRepository.findByUid(uid);
+        return mapUserToUserResponse(user);
+    }
+
+    public UserResponse updateUser(UserRequest request, Long uid) {
+        User user = userRepository.findByUid(uid);
+        User newUser = new User();
+        if (user != null) {
+            newUser = User.builder()
+                    .name(request.getName())
+                    .email(request.getEmail())
+                    .password(request.getPassword())
+                    .role(request.getRole())
+                    .isEnabled(request.getIsEnabled())
+                    .build();
+            userRepository.save(newUser);
+        }
+        return mapUserToUserResponse(newUser);
+    }
+
+    @Override
+    public boolean deleteProduct(Long uid) {
+        User user = userRepository.findByUid(uid);
+
+        if (user != null) {
+            //orderRepository.deleteByUserUid(uid);
+            userRepository.deleteByUid(uid);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private UserResponse mapUserToUserResponse(User user) {
+        return UserResponse.builder()
+                .uid(user.getUid())
+                .name(user.getName())
+                .email(user.getEmail())
+                .role(user.getRole())
+                .isEnabled(user.getIsEnabled())
+                .build();
+    }
+
+}
