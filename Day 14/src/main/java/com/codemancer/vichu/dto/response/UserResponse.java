e5f8dbacdb194f650276b10b8716157e28e68870
vijayaprabha2003 <104961158+vijayaprabha2003@users.noreply.github.com>
2023-10-05 14:37:42 +0530

    Add files via upload

---
+++ Day 14/src/main/java/com/codemancer/vichu/dto/response/UserResponse.java
@@ -0,0 +1,20 @@
+package com.codemancer.vichu.dto.response;
+
+import com.codemancer.vichu.model.enumerate.Role;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+    private Long uid;
+    private String name;
+    private String email;
+    private Boolean isEnabled;
+    private Role role;
+}
