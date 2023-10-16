0528df2a4894842a6029124392522e877b5ce1c7
vijayaprabha2003 <104961158+vijayaprabha2003@users.noreply.github.com>
2023-10-16 12:40:48 +0530

    Add files via upload

---
+++ Day 21/ecom-main/src/main/java/com/codemancer/vichu/dto/request/UserRequest.java
@@ -0,0 +1,20 @@
+package com.codemancer.vichu.dto.request;
+
+import com.codemancer.vichu.model.enumerate.Role;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRequest {
+    private String user;
+    private String email;
+    private String password;
+    private Boolean isEnabled;
+    private Role role;
+}
